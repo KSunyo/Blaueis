dba43443f911068722d3e9576a5319aa1fc25dd2
bemantec <amentbence@gmail.com>
2022-05-24 12:23:41 +0200

    Added label and kind props to Button

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import Button from "./Button";
+import Button, {Kind} from "./Button";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "blaueis/Button",
   component: Button,
+  argTypes: {
+    kind: {
+      control: {
+        type: "radio",
+      },
+      defaultValue: Kind.PRIMARY
+    }
+  }
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -15,6 +23,7 @@ export const HelloWorld = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 HelloWorld.args = {
   label: "Hello world!",
+  kind: Kind.SECONDARY
 };
 
 export const ClickMe = Template.bind({});
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Button.scss";
 
+export enum Kind {
+  PRIMARY = "PRIMARY",
+  SECONDARY = "SECONDARY",
+  TERTIARY = "TERTIARY"
+}
+
 export interface ButtonProps {
+  kind: Kind;
   label: string;
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+const Button = ({label, kind = Kind.PRIMARY}: ButtonProps) => {
+  return <button className={kindToClassName(kind)}>{label}</button>;
 };
+
+const kindToClassName = (kind) => {
+  if (kind == Kind.PRIMARY) {
+    return "primary";
+  } else if (kind == Kind.SECONDARY) {
+    return "secondary";
+  } else if (kind == Kind.TERTIARY) {
+    return "tertiary";
+  }
+}
 
 export default Button;
