2f31e9c3082d60107212aa13b43c20c0407a6e26
bemantec <amentbence@gmail.com>
2022-06-08 18:30:12 +0200

    Added more icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -4,17 +4,26 @@ const UpArrowLight = require("./icons/arrow-up-light.svg") as string;
 const UpArrowDark = require("./icons/arrow-up-dark.svg") as string;
 const RightArrowLight = require("./icons/arrow-right-light.svg") as string;
 const RightArrowDark = require("./icons/arrow-right-dark.svg") as string;
+const LeftArrowLight = require("./icons/arrow-left-light.svg") as string;
+const LeftArrowDark = require("./icons/arrow-left-dark.svg") as string;
 const DownArrowLight = require("./icons/arrow-down-light.svg") as string;
 const DownArrowDark = require("./icons/arrow-down-dark.svg") as string;
 const UpRightArrowLight = require("./icons/arrow-up-right-light.svg") as string;
 const UpRightArrowDark = require("./icons/arrow-up-right-dark.svg") as string;
+const MaximizeLight = require("./icons/maximize-light.svg") as string;
+const MaximizeDark = require("./icons/maximize-dark.svg") as string;
+const MinimizeLight = require("./icons/minimize-light.svg") as string;
+const MinimizeDark = require("./icons/minimize-dark.svg") as string;
 
 
 export enum IconType {
 	UP_ARROW = "UP_ARROW",
   	RIGHT_ARROW = "RIGHT_ARROW",
   	DOWN_ARROW = "DOWN_ARROW",
-  	UP_RIGHT_ARROW = "UP_RIGHT_ARROW"
+	LEFT_ARROW = "LEFT_ARROW",
+  	UP_RIGHT_ARROW = "UP_RIGHT_ARROW",
+	MAXIMIZE = "MAXIMIZE",
+	MINIMIZE = "MINIMIZE"
 }
 
 export enum IconKind {
@@ -52,6 +61,24 @@ const Icon = ({type, kind, alt = ""}: IconProps) : (JSX.Element | null) => {
 			return <img src={UpArrowLight} alt={alt}/>;
 		} else if (kind == IconKind.DARK) {
 			return <img src={UpArrowDark} alt={alt}/>;
+		}
+	} else if (type == IconType.LEFT_ARROW) {
+		if (kind == IconKind.LIGHT) {
+			return <img src={LeftArrowLight} alt={alt}/>;
+		} else if (kind == IconKind.DARK) {
+			return <img src={LeftArrowDark} alt={alt}/>;
+		}
+	} else if (type == IconType.MAXIMIZE) {
+		if (kind == IconKind.LIGHT) {
+			return <img src={MaximizeLight} alt={alt}/>;
+		} else if (kind == IconKind.DARK) {
+			return <img src={MaximizeDark} alt={alt}/>;
+		}
+	} else if (type == IconType.MINIMIZE) {
+		if (kind == IconKind.LIGHT) {
+			return <img src={MinimizeLight} alt={alt}/>;
+		} else if (kind == IconKind.DARK) {
+			return <img src={MinimizeDark} alt={alt}/>;
 		}
 	}
 	return null;
