57c2e5fed1740100cf811c308ebcad550c2fcb53
bemantec <amentbence@gmail.com>
2022-08-10 16:59:07 +0200

    Update Stack.stories.tsx

--- src/components/Stack/Stack.stories.tsx
+++ src/components/Stack/Stack.stories.tsx
@@ -4,29 +4,29 @@ import Stack, { ORIENTATIONS } from "./Stack";
 import Button, { KINDS } from "../Button/Button";
 
 export default {
-  title: "blaueis/Stack",
-  component: Stack,
-  argTypes: { 
-    spacing: { 
-      control: { 
-        type: 'range', 
-        min:1, 
-        max:100, 
-        step: 1 
-      } 
+    title: "blaueis/Stack",
+    component: Stack,
+    argTypes: {
+        spacing: {
+            control: {
+                type: 'range',
+                min:1,
+                max:100,
+                step: 1
+            }
+        }
     }
-  }
 } as ComponentMeta<typeof Stack>;
 
 const Template: ComponentStory<typeof Stack> = (args) => <Stack {...args} />;
 
 export const ButtonGroup = Template.bind({});
 ButtonGroup.args = {
-  orientation: ORIENTATIONS.Row,
-  spacing: 10,
-  children: [(<Button kind={KINDS.Primary} label="Button 1"/>),
-    (<Button kind={KINDS.Primary} label="Button 2"/>),
-    (<Button kind={KINDS.Primary} label="Button 3"/>),
-    (<Button kind={KINDS.Primary} label="Button 4"/>),
-    (<Button kind={KINDS.Primary} label="Button 5"/>)]
+    orientation: ORIENTATIONS.Row,
+    spacing: 10,
+    children: [(<Button kind={KINDS.Primary} label="Button 1"/>),
+        (<Button kind={KINDS.Primary} label="Button 2"/>),
+        (<Button kind={KINDS.Primary} label="Button 3"/>),
+        (<Button kind={KINDS.Primary} label="Button 4"/>),
+        (<Button kind={KINDS.Primary} label="Button 5"/>)]
 };
