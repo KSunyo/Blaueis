4cb4bceaa92e8e7c2e2ba87b74a687e6170151b8
bemantec <amentbence@gmail.com>
2022-08-10 17:01:56 +0200

    Adjusted Stack story controls

--- src/components/Stack/Stack.stories.tsx
+++ src/components/Stack/Stack.stories.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Stack, { ORIENTATIONS } from "./Stack";
-import Button, { KINDS } from "../Button/Button";
+import Button, {KINDS, SIZES} from "../Button/Button";
+import {IconType} from "../Icon/Icon";
 
 export default {
     title: "blaueis/Stack",
@@ -14,7 +15,21 @@ export default {
                 max:100,
                 step: 1
             }
-        }
+        },
+        orientation: {
+            control: {
+                type: "select",
+                labels: {
+                    1: "Row",
+                    2: "Row-reverse",
+                    3: "Column",
+                    4: "Column-reverse"
+                },
+            },
+            options: Object.keys(ORIENTATIONS),
+            mapping: ORIENTATIONS,
+            defaultValue: ORIENTATIONS.Row,
+        },
     }
 } as ComponentMeta<typeof Stack>;
 
